cf09cfbae3852c21664e4641c06350a04af80c83
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-05-05 14:43:44 +0300

    feature: made minor changes in a project
    - added some links on the main page
    - created echo endpoint

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,8 +1,19 @@
+from dataclasses import dataclass
+
 from fastapi import APIRouter
 
 router = APIRouter()
 
 
+@dataclass
+class EchoDto:
+    text: str
+
+
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.post('')
+def echo(dto: EchoDto) -> dict:
+    return {'msg': dto.text}
